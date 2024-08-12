450601f2229083a4a922d933b5a524bb852e77fa
difool95 <65021359+difool95@users.noreply.github.com>
2024-08-12 11:11:50 +0100

    Update dash.js

--- public/dash.js
+++ public/dash.js
@@ -230,8 +230,8 @@ function edit_row(r)
 function add_row()
 {
     var table = document.getElementById("user_table");
-    var row = table.insertRow(rows.length + 1);
-    
+    //var row = table.insertRow(rows.length + 1);
+    var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
